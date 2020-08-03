eb81785723d970df258876ff3bedd639069ee045
Leonardo Zangrando <lzangrando@gmail.com>
2020-08-03 10:52:43 +0100

    added unsplash images

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SlideShow from '../components/SlideShow';
 
 var settings = {
   images: [
-    { url: require('../assets/images/bg01.jpg'), position: 'center' },
-    { url: require('../assets/images/bg02.jpg'), position: 'center' },
-    { url: require('../assets/images/bg03.jpg'), position: 'center' },
+    { url: require('https://source.unsplash.com/1600x900/?ocean'), position: 'center' },
+    { url: require('https://source.unsplash.com/1600x900/?ocean'), position: 'center' },
+    { url: require('https://source.unsplash.com/1600x900/?ocean'), position: 'center' },
   ],
   // Delay.
   delay: 6000,
