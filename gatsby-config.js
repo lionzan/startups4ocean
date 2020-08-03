e448e244c52f4e52b8898e27cc5d7e7651fd40f8
lionzan <me@lionzan.me>
2020-08-03 20:39:06 +0100

    added seo module

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,28 @@ module.exports = {
         trackingId: "UA-174294648-1",
       },
     },
+    {
+      resolve: 'gatsby-plugin-next-seo',
+      options: {
+        openGraph: {
+          type: 'website',
+          locale: 'en_GB',
+          url: 'https://startups4ocean.com/',
+          site_name: 'Startups 4 Ocean',
+          title: 'pre-registration landing page',
+          titleTemplate: 'Startups 4 Ocean | ',
+          description: 'The Global Virtual Hub of Startup-driven Ocean Innovation',
+          images: [
+            {
+              url: 'https://startups4ocean.com/static/bg17-8ecf3a295b195c1f6b43dd4842b01f1a.jpg',
+              width: 1920,
+              height: 1080,
+              alt: 's4o turtle',
+            },
+          ],
+        },
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
